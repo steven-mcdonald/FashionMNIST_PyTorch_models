3d3088a30efccf0556e3f74be5e5708a4ea5511f
Steven McDonald <45227692+steven-mcdonald@users.noreply.github.com>
2023-03-10 14:37:55 +0000

    start on image plotting

--- data_downloader.py
+++ data_downloader.py
@@ -1,5 +1,7 @@
 # conda install pytorch using $ conda install pytorch torchvision -c pytorch
 
+import matplotlib.pyplot as plt
+
 import torch
 import torchvision
 from torchvision import transforms, datasets
@@ -8,6 +10,8 @@ import torch.nn as nn
 # functional aspects of pytorch (interchangeable with above just a funct approach rather than object approach)
 import torch.nn.functional as F
 import torch.optim as optim
+
+from image_plotter import data_loader_img_view
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,7 +25,7 @@ np.random.seed(seed_num)
 def get_fashion_dataset():
     # load in the training and test set for Fashion MNIST using torchvision datasets function.
     # this puts the data into a nice format for batching later on.
-    train_set = datasets.FashionMNIST(
+    train_dev_set = datasets.FashionMNIST(
         root="./data/input/",
         train=True,
         download=True,
@@ -35,9 +39,23 @@ def get_fashion_dataset():
 
     # split the training data further into training and validation sets.
     # split made reproducible  with the random seed.
-    train_set, dev_set = torch.utils.data.random_split(train_set, [50000, 10000], generator=torch.Generator().manual_seed(42))
+    train_set, dev_set = torch.utils.data.random_split(train_dev_set, [50000, 10000], generator=torch.Generator().manual_seed(42))
 
     return train_set, dev_set, test_set
+
+
+class DatasetTransformer(torch.utils.data.Dataset):
+
+    def __init__(self, base_dataset, transform):
+        self.base_dataset = base_dataset
+        self.transform = transform
+
+    def __getitem__(self, index):
+        img, target = self.base_dataset[index]
+        return self.transform(img), target
+
+    def __len__(self):
+        return len(self.base_dataset)
 
 
 def create_dataloader(data_set, batch_size=32):
@@ -52,18 +70,48 @@ if __name__ == '__main__':
     device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
     print(f"Using {device} device")
 
-    train, dev, test = get_fashion_dataset()
+    train_dataset, dev_dataset, test_dataset = get_fashion_dataset()
 
-    # Load the training data as a full batch
-    train_fullbatch = create_dataloader(train, batch_size=len(train))
-    # from the full batch data extract only the images without the labels
-    # these will be the target images
-    for data in train_fullbatch:
-        train_fullbatch_X, _ = data
+    # train_dataset = DatasetTransformer(train_dataset, transforms.ToTensor())
+    # valid_dataset = DatasetTransformer(dev_dataset, transforms.ToTensor())
+    # test_dataset = DatasetTransformer(test_dataset, transforms.ToTensor())
+
+    # Dataloaders
+    num_threads = 4  # Loading the dataset is using 4 CPU threads
+    batch_size = 128  # Using minibatches of 128 samples
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=True,  # <-- this reshuffles the data at every epoch
+                                               num_workers=num_threads)
+
+    dev_loader = torch.utils.data.DataLoader(dataset=dev_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=False,
+                                               num_workers=num_threads)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False,
+                                              num_workers=num_threads)
+
+    print("The train set contains {} images, in {} batches".format(len(train_loader.dataset), len(train_loader)))
+    print("The validation set contains {} images, in {} batches".format(len(dev_loader.dataset), len(dev_loader)))
+    print("The test set contains {} images, in {} batches".format(len(test_loader.dataset), len(test_loader)))
+
+    for i, data in enumerate(train_loader):
+        print(i)
+        X_sample, y_sample = data
         break
 
+    nsamples = 10
+    label_keys = [x for x in range(10)]
+    classes_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag',
+                     'Ankle boot']
+    label_dict = dict(zip(label_keys, classes_names))
 
-    plt.imshow(train_fullbatch_X[0].view(28, 28))
-    plt.show()
+    labels = [label_dict[k] for k in y_sample.tolist()]
+
+    data_loader_img_view(X_sample, labels, nsamples)
 
     print()
---
+++ image_plotter.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+def data_loader_img_view(images, labels, nsamples=1):
+
+    fig = plt.figure(figsize=(20, 5), facecolor='w')
+    for i in range(nsamples):
+        ax = plt.subplot(1, nsamples, i+1)
+        plt.imshow(images[i, 0, :, :], vmin=0, vmax=1.0, cmap=cm.gray)
+        ax.set_title(f"{labels[i]}", fontsize=15)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+    plt.savefig('fashionMNIST_samples.png', bbox_inches='tight')
+    plt.show()
